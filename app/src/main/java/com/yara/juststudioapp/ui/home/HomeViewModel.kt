ab8378fdf103aafe2b878fce01842344cc2dffed
yaroslavss <yaroslav.syuzev@gmail.com>
2023-11-11 14:52:37 +0400

    Finish home fragment draft

--- app/src/main/java/com/yara/juststudioapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/yara/juststudioapp/ui/home/HomeViewModel.kt
@@ -6,8 +6,14 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _text_address = MutableLiveData<String>().apply {
+        value = "Адрес, город, улица, дом"
     }
-    val text: LiveData<String> = _text
+    val text_address: LiveData<String> = _text_address
+
+    private val _text_phone = MutableLiveData<String>().apply {
+        value = "+7 (495) 788-77-50"
+    }
+    val text_phone: LiveData<String> = _text_phone
+
 }
